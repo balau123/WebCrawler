4b7147c08bd9b88341794ec4b6abc936977309e6
balau123 <thai.itplus@gmail.com>
2018-05-08 10:01:01 -0700

    fix bugs

--- getNews.rb
+++ getNews.rb
@@ -1,57 +1,49 @@
 require_relative "News/News"
+require 'rss'
+require 'open-uri'
 
-class GetNews < News
+class GetNews
   def initialize()
-    require 'rss'
-    require 'open-uri'
     @rss_results = []
+		@n=News.new()
   end
-  
-  def vnexpress(type=:tintuc,count=5)
-    rss = RSS::Parser.parse(open(@@list_vnexpress_category[type]).read, false).items[0..count]
-    rss.each do |result|
-      d=result.description
-      image=d.scan(/src(.*)[=](.*)["' ']>/)
-      content=d.scan(/br>(.*)/)
-      image=image.join('')
-      content=content.join('')  
-      r = { title: result.title, date: result.pubDate, link: result.link, content: content, img: image }
-      @rss_results.push(r)
-    end 
-  end
-  
-  def show_vnexpress()
-    @rss_results.map.with_index { |r,index|
-      puts "\n%d\n| Title: %s \n Date: %s \n Link: %s \n Img:%s \nDescription: %s" % [index,r[:title],r[:date],r[:link],r[:img],r[:content]]
-    }
-  end
+
 	#===============================================================
-
-  def dantri(type=:tintuc,count=5)
-    rss = RSS::Parser.parse(open(@@list_dantri_category[type]).read, false).items[0..count]
-    rss.each do |result|
-      d=result.description
-      image=d.scan(/src(.*)[=](.*)["' ']>/)
-      content=d.scan(/br>(.*)/)
-      image=image.join('')
-      content=content.join('')  
-      r = { title: result.title, date: result.pubDate, link: result.link, content: content, img: image }
-      @rss_results.push(r)
+  def craw(_page,_type=:tintuc,count=5)
+		@page=getPage(_page,_type)
+		begin
+    	rss = RSS::Parser.parse(open(@page).read, false).items[0..count]
+		rescue
+			puts "[Link Died]: #@page"
+		ensure
+		  rss.each do |result|
+		    d=result.description
+		    image=d.scan(/src(.*)[=](.*)["' ']/)
+		    content=d.scan(/br>(.*)/)
+		    image=image.join('')
+		    content=content.join('')  
+		    r = { title: result.title, date: result.pubDate, link: result.link, content: content, img: image }
+		    @rss_results.push(r)
+			end
     end 
   end
 
-  def show_dantri()
+  def show()
     @rss_results.map.with_index { |r,index|
-      puts "\n%d\n| Title: %s \n Date: %s \n Link: %s \n Img:%s \nDescription: %s" % [index,r[:title],r[:date],r[:link],r[:img],r[:content]]
+      puts "\n%d\n|Title: %s \n|Date: %s \n|Link: %s \n|Img:%s \n|Content: %s" % [index,r[:title],r[:date],r[:link],r[:img],r[:content]]
     }
 	end
   
+	def getPage(_page,_type)
+		@n.pages[_page][_type]
+	end
+
 end
 
 n=GetNews.new()
-#n.vnexpress(:thethao,20)
-#n.show_vnexpress()
-n.dantri(:thoisu,20)
-n.show_dantri()
+#puts n.getPage('vtc',:tintuc)
+#n.craw('vtc',:tintuc	,10)
+n.craw('vtc',:khoahoc	,10)
+n.show()
 
 
