71e2b22687dfd40120a9b9c2aa5dfe26650aabce
balau123 <thai.itplus@gmail.com>
2018-05-12 09:26:22 +0700

    Master

--- test/controllers/craw_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CrawControllerTest < ActionDispatch::IntegrationTest
-  test "should get news" do
-    get craw_news_url
-    assert_response :success
-  end
-
-end
--- test/controllers/goodbye_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GoodbyeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
