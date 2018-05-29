fe04550667dbd279e8b5be695dc01577eb32ac83
balau123 <thai.itplus@gmail.com>
2018-05-29 16:55:00 +0700

    Fix view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap
+//= require activestorage
+//= require_tree .
+
 
 
